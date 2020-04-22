30bae17650d1234f4c78d1c6f33e25f0198bb2dd
admin <admin@admins-MacBook-Pro.local>
2020-04-22 10:26:15 -0700

    Updates

---
+++ precache-manifest.f2375ef50fc8bafd27b3de0aff1902c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93bf4ebf9cbe92fd0c7be882717b0cc4",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "bfa4efaf6cb0b104933c",
+    "url": "/hello/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "df2878ee6478190bc91e",
+    "url": "/hello/static/js/2.62c6fed1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello/static/js/2.62c6fed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa4efaf6cb0b104933c",
+    "url": "/hello/static/js/main.27e8c0df.chunk.js"
+  },
+  {
+    "revision": "e50dd2d5254e5efcd853",
+    "url": "/hello/static/js/runtime-main.b04953e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  }
+]);
